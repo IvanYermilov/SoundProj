d2b883fa4b063366561f71a2005bc0680aa0e94a
Ivan Yermilov <van.ermilov@gmail.com>
2023-06-16 23:24:47 +0300

    Fixed sound absorbtion

--- Assets/SoundAbsorber.cs
+++ Assets/SoundAbsorber.cs
@@ -7,21 +7,21 @@ public class SoundAbsorber : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var characterPosition = GameObject.Find("Character").transform.position;
+        var characterPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
 
-        if (Physics.Raycast(transform.position, characterPosition, Mathf.Infinity))
+        if (!Physics.Linecast(transform.position, characterPosition))
         {
             var carAudio = GetComponent<AudioSource>();
             carAudio.volume = 1.0f;
-            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            //Debug.Log("Did Hit");
+            Debug.DrawLine(transform.position, characterPosition, Color.red);
+            Debug.Log("Did Hit");
         }
         else
         {
-            var carAudio = GetComponent<AudioSource>(); 
+            var carAudio = GetComponent<AudioSource>();
             carAudio.volume = 0.5f;
-            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            //Debug.Log("Did not Hit");
+            Debug.DrawLine(transform.position, characterPosition, Color.red);
+            Debug.Log("Did not Hit");
         }
 
     }
